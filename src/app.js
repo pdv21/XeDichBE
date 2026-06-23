f2c1cf0818a539964426638d6239baa785f6afce
jeedes <Annq.b22cn050@stu.ptit.edu.vn>
2026-06-23 17:59:18 +0700

    feat: add user module APIs

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const authRoutes = require('./modules/auth/auth.route');
+const userRoutes = require('./modules/user/user.route');
 
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 
 module.exports = app;
