878fff395c15da85870bb10fb6fc5f0c4e0cdebb
jeedes <Annq.b22cn050@stu.ptit.edu.vn>
2026-06-24 13:46:32 +0700

    Configure user routes

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -2,16 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./user.controller');
 
-// Lấy danh sách user
 router.get('/', userController.getAllUsers);
-
-// Tìm kiếm user theo tên
 router.get('/search', userController.searchUsers);
-
-// Lấy thông tin 1 user theo id
 router.get('/:id', userController.getUserById);
-
-// Cập nhật thông tin user
 router.put('/:id', userController.updateUser);
 
 module.exports = router;
